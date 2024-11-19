8b503e0e19bfb7f0891b9f6fd0c67e907ad14932
amaleshayo <165439702+amaleshayo@users.noreply.github.com>
2024-11-19 12:44:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('bx-x');
+  navBar.classList.toggle('active');
+});
